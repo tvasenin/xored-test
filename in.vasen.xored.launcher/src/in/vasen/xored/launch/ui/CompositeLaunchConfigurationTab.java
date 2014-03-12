fc8dc377ff113e8dcec04db740fc7fd81b0dd651
Timofey Vasenin <timofey.vasenin@gmail.com>
2014-03-12 23:06:36 +0700

    Add Tab UI stub

--- in.vasen.xored.launcher/src/in/vasen/xored/launch/ui/CompositeLaunchConfigurationTab.java
+++ in.vasen.xored.launcher/src/in/vasen/xored/launch/ui/CompositeLaunchConfigurationTab.java
@@ -4,15 +4,41 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
 
 public class CompositeLaunchConfigurationTab extends
 		AbstractLaunchConfigurationTab implements ILaunchConfigurationTab {
 
+	private TableViewer tableViewer;
+	
 	@Override
 	public void createControl(Composite parent) {
-		// TODO Auto-generated method stub
+		Composite comp = new Composite(parent, SWT.NONE);
+		comp.setLayout(new GridLayout(2, false));
 
+		tableViewer = new TableViewer(comp, SWT.SINGLE);
+		Table table = tableViewer.getTable();
+		table.setHeaderVisible(true);
+		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		Composite buttons = new Composite(comp, SWT.None);
+		buttons.setLayout(new GridLayout());
+
+		Button addButton = new Button(buttons, SWT.PUSH);
+		addButton.setText("Add...");
+		addButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		Button removeButton = new Button(buttons, SWT.PUSH);
+		removeButton.setText("Remove");
+		removeButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+				
+		setControl(comp);
 	}
 
 	@Override
