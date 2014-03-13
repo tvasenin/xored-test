36c96702f6a70f8b85ef169d1fddf3912edc6c6b
Timofey Vasenin <timofey.vasenin@gmail.com>
2014-03-14 01:41:55 +0700

    Fetch launch configs

--- in.vasen.xored.launcher/src/in/vasen/xored/launch/CompositeLaunchConfigurationDelegate.java
+++ in.vasen.xored.launcher/src/in/vasen/xored/launch/CompositeLaunchConfigurationDelegate.java
@@ -1,7 +1,10 @@
 package in.vasen.xored.launch;
 
+import java.util.ArrayList;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
@@ -12,8 +15,28 @@ public class CompositeLaunchConfigurationDelegate implements
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		// TODO Auto-generated method stub
-		
+		@SuppressWarnings("unchecked")
+		ArrayList<String> items = (ArrayList<String>) configuration.getAttribute("items", new ArrayList<String>());
+		ArrayList<ILaunchConfiguration> configs = fetchLaunchConfigurations(items, mode);
+		for (ILaunchConfiguration config : configs) {
+//			config.launch(mode, monitor);
+		}
 	}
-
+	
+	private ArrayList<ILaunchConfiguration> fetchLaunchConfigurations(ArrayList<String> names, String mode) {
+		ArrayList<ILaunchConfiguration> launchConifigs = new ArrayList<ILaunchConfiguration>();
+		for (String name : names) {
+			try {
+				ILaunchConfiguration[] configs = DebugPlugin.getDefault().getLaunchManager().getLaunchConfigurations();
+				for (ILaunchConfiguration config : configs) {
+					if (config.supportsMode(mode) && config.getName().equals(name)) {
+						launchConifigs.add(config);
+					}
+				}
+			} catch (CoreException e) {
+			}
+		}
+		return launchConifigs;
+	}
+	
 }
